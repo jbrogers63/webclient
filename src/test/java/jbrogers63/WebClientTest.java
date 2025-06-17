7c8b37cb2960914e913a886b79c221382af16465
Jeff Rogers <jbrogers63@gmail.com>
2025-06-17 10:16:22 -0400

    Round out tests.

--- src/test/java/jbrogers63/WebClientTest.java
+++ src/test/java/jbrogers63/WebClientTest.java
@@ -1,7 +1,9 @@
 package jbrogers63;
 
+import java.net.http.HttpHeaders;
 import java.net.http.HttpResponse;
 import java.util.Map;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -25,6 +27,11 @@ public class WebClientTest {
 				switch (request.getPath()) {
 					case "/good":
 						return getMockResponse("OK", 200, Map.of());
+					case "/bad":
+						return getMockResponse("", 500, Map.of());
+					case "/json":
+						return getMockResponse("{ \"test\": \"data\" }", 200,
+								Map.of("Content-Type", "application/json"));
 					default:
 						return new MockResponse().setResponseCode(404);
 				}
@@ -47,6 +54,42 @@ public class WebClientTest {
 
 		Assertions.assertEquals("OK", result.body(), "The response body did not match.");
 		Assertions.assertEquals(200, result.statusCode(), "The response status code did not match.");
+	}
+
+	@Test
+	public void testServerError() throws Exception {
+		String url = server.url("/bad").toString();
+		WebClient client = new WebClient();
+		HttpResponse<String> result = client.get(url, Map.of("Content-Type", "plain/text"));
+
+		Assertions.assertEquals("", result.body(), "The response body did not match.");
+		Assertions.assertEquals(500, result.statusCode(), "The response status code did not match.");
+	}
+
+	@Test
+	public void testNotFound() throws Exception {
+		String url = server.url("/nonexist").toString();
+		WebClient client = new WebClient();
+		HttpResponse<String> result = client.get(url, Map.of());
+
+		Assertions.assertEquals("", result.body(), "The response body did not match.");
+		Assertions.assertEquals(404, result.statusCode(), "The response status code did not match.");
+	}
+
+	@Test
+	public void testJsonResponse() throws Exception {
+		String url = server.url("/json").toString();
+		WebClient client = new WebClient();
+		HttpResponse<String> result = client.post(url, "{ \"hello\": \"world\" }",
+				Map.of("Content-Type", "application/json", "Accept", "application/json"));
+
+		HttpHeaders headers = result.headers();
+		String headerResult = headers.firstValue("content-type").get();
+
+		Assertions.assertEquals("{ \"test\": \"data\" }", result.body(), "The response body did not match.");
+		Assertions.assertEquals(200, result.statusCode(), "The response status code did not match.");
+		Assertions.assertEquals("application/json", headerResult,
+				"The response status code did not match.");
 	}
 
 	private MockResponse getMockResponse(String body, int responseCode, Map<String, String> headers) {
